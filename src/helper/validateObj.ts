0e8ff3e8bc898d1fab5ff5208e7ad15ee2ead42f
karosi12 <adekayor@gmail.com>
2020-11-21 23:46:40 +0100

    added validator

---
+++ src/helper/validateObj.ts
@@ -0,0 +1,26 @@
+import * as Joi from "joi";
+import { responsesHelper } from "../utils/responses";
+
+const screenShotJoi = async (req, res, next) => {
+  try {
+    const data = await Joi.validate(req.body, screenShotSchema);
+    if (data) return next();
+  } catch (error) {
+    if (error.details) {
+      const errDetails = error.details.map((i) => ({
+        message: i.message.replace(/['"]/g, ""),
+      }));
+      return res
+        .status(400)
+        .send(responsesHelper.error(400, errDetails[0].message));
+    }
+    return res.status(500).send(responsesHelper.error(500, error.message));
+  }
+};
+
+const screenShotSchema = Joi.object().keys({
+  websiteName: Joi.string().required(),
+  url: Joi.string().uri().required(),
+});
+
+export default { screenShotJoi };
